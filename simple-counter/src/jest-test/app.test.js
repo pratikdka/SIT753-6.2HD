6dd077e799c25fcf1a3dcb13d977d7082a088a6f
pratikdka <pratik.khadka18@gmail.com>
2024-04-22 02:18:18 +1000

    Fixed ESLint issues

--- simple-counter/src/jest-test/app.test.js
+++ simple-counter/src/jest-test/app.test.js
@@ -1,33 +1,33 @@
-import {render, screen, fireEvent} from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from '../App';
 
 describe('App component', () => {
     test('increments counter', () => {
         render(<App />);
-            const incrementButton = screen.getByText('+');
-            fireEvent.click(incrementButton);
-            const input = screen.getByDisplayValue('1');
-            expect(input).toHaveValue("1");
+        const incrementButton = screen.getByText('+');
+        fireEvent.click(incrementButton);
+        const input = screen.getByDisplayValue('1');
+        expect(input).toHaveValue("1");
     });
 
     test('decrements counter', () => {
         render(<App />);
-            const incrementButton = screen.getByText('+');
-            fireEvent.click(incrementButton);
-            const decrementButton = screen.getByText('-');
-            fireEvent.click(decrementButton);
-            const input = screen.getByDisplayValue('0');
-            expect(input).toHaveValue("0");
+        const incrementButton = screen.getByText('+');
+        fireEvent.click(incrementButton);
+        const decrementButton = screen.getByText('-');
+        fireEvent.click(decrementButton);
+        const input = screen.getByDisplayValue('0');
+        expect(input).toHaveValue("0");
     });
 
     test('clears counter', () => {
         render(<App />);
-            const incrementButton = screen.getByText('+');
-            fireEvent.click(incrementButton);
-            fireEvent.click(incrementButton);
-            const clearButton = screen.getByText('Clear');
-            fireEvent.click(clearButton);
-            const input = screen.getByDisplayValue('0');
-            expect(input).toHaveValue("0");
+        const incrementButton = screen.getByText('+');
+        fireEvent.click(incrementButton);
+        fireEvent.click(incrementButton);
+        const clearButton = screen.getByText('Clear');
+        fireEvent.click(clearButton);
+        const input = screen.getByDisplayValue('0');
+        expect(input).toHaveValue("0");
     });
 });
