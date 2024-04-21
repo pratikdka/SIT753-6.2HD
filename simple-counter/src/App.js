c7be5dff154b37e755e3a9da3767011f9ea34dfc
pratikdka <pratik.khadka18@gmail.com>
2024-04-21 20:23:28 +1000

    Simple Project for SIT753

---
+++ simple-counter/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const clear = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <div className="counter">
+        <button onClick={decrement}>-</button>
+        <input type="text" value={count} readOnly />
+        <button onClick={increment}>+</button>
+      </div>
+      <button onClick={clear} className="clear-button">
+        Clear
+      </button>
+    </div>
+  );
+}
+
+export default App;
