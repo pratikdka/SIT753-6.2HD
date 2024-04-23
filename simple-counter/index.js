595c3b34418db5c824aa5e2b76a8f07724feb3a5
pratikdka <pratik.khadka18@gmail.com>
2024-04-24 00:59:09 +1000

    Added Deployment Stage to project file

--- simple-counter/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-// Define your routes and middleware here
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
