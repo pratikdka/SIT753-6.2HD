6d06d7b12dd0804c04cc65cfc6cdad05690a517a
Pratik Khadka <pratik.khadka18@gmail.com>
2024-05-01 23:42:04 +1000

    Updated the changes

--- simple-counter/src/jest-test/app.test.js
+++ simple-counter/src/jest-test/app.test.js
@@ -10,7 +10,7 @@ describe('App component', () => {
 		render(<App />);
 		const incrementButton = screen.getByText('+');
 		fireEvent.click(incrementButton);
-		const input = screen.getByDisplayValue('1');
+		const input = screen.getByDisplayValue('2');
 		expect(input).toHaveValue('1');
 	});
 
